9759d35101a0cbd60042a693ebe58efe89ea9e81
AhmedAD251 <ahmedabdaldayim251@gmail.com>
2023-09-08 16:22:25 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: single digits nums from 0 to 10
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+{
+		printf("%i", num);
+		num++;
+}
+	printf("\n");
+	return (0);
+}
