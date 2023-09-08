b526fb3f85c564747148c52dc3fce5eac3278c63
AhmedAD251 <ahmedabdaldayim251@gmail.com>
2023-09-07 19:52:38 -0700

    C Prep

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * description: A C program using sizeof fonction to print different kinds of data
+ * description: A C program using sizeof fonction to print data
  *
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -15,5 +16,4 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-
 }
