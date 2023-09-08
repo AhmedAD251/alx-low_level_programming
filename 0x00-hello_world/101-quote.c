efecd4330e09dbb659964234d2e60823ccef9c3f
AhmedAD251 <ahmedabdaldayim251@gmail.com>
2023-09-07 20:10:57 -0700

    C Prep

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,16 +3,13 @@
 /**
  * main - Entry point
  *
- * description: print a qoute
+ * description: A C program that prints a qoute
  *
  * Return: 1 (fail)
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, quo, 59);
-	
 	return (1);
 }
